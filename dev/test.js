318796e23949cffb8c6d7cf168db7c41da7da57e
alansarluv <alan.sarluf@gmail.com>
2021-05-25 00:24:28 +0700

    Add test

--- dev/test.js
+++ dev/test.js
@@ -1,17 +1,37 @@
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389, '0INA90SDNF90N', '90ANSD9F0N900N');
+// bitcoin.createNewBlock(2389, '0INA90SDNF90N', '90ANSD9F0N900N');
 
-bitcoin.createNewTransaction(100, 'ALEXJUYTLK', 'JANEJIUY878HGJK');
-bitcoin.createNewBlock(1111, '0INHGSDNF90N', '90ANSNMBH0N');
+// bitcoin.createNewTransaction(100, 'ALEXJUYTLK', 'JANEJIUY878HGJK');
+// bitcoin.createNewBlock(1111, '0INHGSDNF90N', '90ANSNMBH0N');
 
-bitcoin.createNewTransaction(2121, 'ALEXJUYTLK1', 'JANEJIUY878HGJK1');
-bitcoin.createNewTransaction(2231, 'ALEXJUYTLK2', 'JANEJIUY878HGJK2');
-bitcoin.createNewTransaction(2412, 'ALEXJUYTLK3', 'JANEJIUY878HGJK3');
-bitcoin.createNewBlock(3212, 'JUGALAIHYU898JH', '987HJAA90ANSNMBH0N');
+// bitcoin.createNewTransaction(2121, 'ALEXJUYTLK1', 'JANEJIUY878HGJK1');
+// bitcoin.createNewTransaction(2231, 'ALEXJUYTLK2', 'JANEJIUY878HGJK2');
+// bitcoin.createNewTransaction(2412, 'ALEXJUYTLK3', 'JANEJIUY878HGJK3');
+// bitcoin.createNewBlock(3212, 'JUGALAIHYU898JH', '987HJAA90ANSNMBH0N');
 
 
+// const previousBlockHash = "JHGUYFTRDRT896VGHHGWK7231B";
+// const currentBlockData = [
+//   {
+//     amount: 10,
+//     sender: 'NAQPFYOW8910JKGIJ767VHJJK',
+//     recipient: 'HJAOJIW782610JHBJBBKKVDFGDGL'
+//   },
+//   {
+//     amount: 30,
+//     sender: 'HAPWJNAQPFY212321KLLAJSKAJ67868K',
+//     recipient: 'ZAPWNLKDWNCX782610JHBJBBKKVVAJHJSW'
+//   },
+//   {
+//     amount: 200,
+//     sender: 'CFGACOIJSOW761027HJKAKJ7KJGHJGS',
+//     recipient: 'KJHAGSIOW1811JKHASUSAK987GHFDAGSJHK'
+//   },
+// ];
 
-console.log(bitcoin);
+// console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+// console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, '251725'));
 
+// console.log(bitcoin)
