176582fa690761dacad0d821edd2602bfd1eef51
alansarluv <alan.sarluf@gmail.com>
2021-05-26 11:28:08 +0700

    Init list of API to build

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello codes World')
-})
+// API for create new block
+app.get('/blockchain', function (req, res) {
 
-app.listen(3000)
+});
+
+// API for create new transaction
+app.get('/transaction', function (req, res) {
+
+});
+
+// API for mining
+app.get('/mine', function (req, res) {
+
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000...');
+})
